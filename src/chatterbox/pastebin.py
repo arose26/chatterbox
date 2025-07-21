4cc3cb2863940ce0681ab274fe6c59ac014dd7da
arose26 <axelrose20262026@gmail.com>
2025-07-21 21:24:27 +0100

    encode it

--- src/chatterbox/pastebin.py
+++ src/chatterbox/pastebin.py
@@ -1,5 +1,26 @@
 import requests
 import xml.etree.ElementTree as ET
+import base64
+
+def encode(text, password):
+    enc = []
+    for i in range(len(text)):
+        key_c = password[i % len(password)]
+        enc_c = chr((ord(text[i]) + ord(key_c)) % 256)
+        enc.append(enc_c)
+    encoded_bytes = "".join(enc).encode()
+    return base64.urlsafe_b64encode(encoded_bytes).decode()
+
+def decode(encoded_text, password):
+    enc = base64.urlsafe_b64decode(encoded_text).decode()
+    dec = []
+    for i in range(len(enc)):
+        key_c = password[i % len(password)]
+        dec_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256)
+        dec.append(dec_c)
+    return "".join(dec)
+
+
 
 api_dev_key = 'HNKedNEoBzYurdcai02udNLBx_rymUys'
 api_user_name = 'arozventi'     
@@ -11,20 +32,17 @@ login_data = {
     'api_user_name': api_user_name,
     'api_user_password': api_user_password
 }
+login_response = requests.post("https://pastebin.com/api/api_login.php", data=login_data)
+api_user_key = login_response.text.strip()
 
-def get_api_user_key():
-    login_response = requests.post("https://pastebin.com/api/api_login.php", data=login_data)
-    api_user_key = login_response.text.strip()
-    return api_user_key
 
 def create_paste(paste_text):
     # Step 2: Create a new paste
-    api_user_key = get_api_user_key()
     data = {
         'api_option': 'paste',
         'api_dev_key': api_dev_key,
         'api_user_key': api_user_key,
-        'api_paste_code': paste_text,
+        'api_paste_code': encode(paste_text, 'gellybean'),
         'api_paste_name': 'Narration Upload',  # Optional: set a title
         'api_paste_private': '1',              # 0=public, 1=unlisted, 2=private
         'api_paste_expire_date': 'N',          # 'N' for never expire
@@ -33,7 +51,6 @@ def create_paste(paste_text):
     print("Paste URL:", response.text)
 
 def get_most_recent_paste():
-    api_user_key = get_api_user_key()
     # Step 1: Fetch a list of pastes
     data = {
         'api_option': 'list',
@@ -65,7 +82,7 @@ def get_most_recent_paste():
     raw_response.raise_for_status()
 
     content = raw_response.text
-    return content
+    return decode(content, 'gellybean')
 
 
 import sys 
